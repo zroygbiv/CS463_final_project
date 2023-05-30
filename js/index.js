31714d9541ac61b51635bd7032be0a4df86e36be
Zach Roth <zach.roth21@gmail.com>
2023-05-30 16:55:11 -0700

    Updated css styling, consolidated html, added JS autoscroll behavior

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Navigation bar page scrolling behavior
+document.querySelectorAll('a[href^="#"]').forEach((anchorTag) => {
+  anchorTag.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
